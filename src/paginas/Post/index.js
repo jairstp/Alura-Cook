378489fcd3065456a034cc82693a43fa9d073025
jair <jairstp@hotmail.com>
2023-05-30 12:09:14 -0300

    first commit

---
+++ src/paginas/Post/index.js
@@ -0,0 +1,43 @@
+import './Post.css';
+
+import { Route, Routes, useParams } from "react-router-dom"
+import posts from "json/posts.json";
+import PostModelo from "componentes/PostModelo";
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+import PaginaPadrao from 'componentes/PaginaPadrao';
+
+export default function Post() {
+    const parametros = useParams();
+
+   const post = posts.find((post) =>{
+      return post.id === Number (parametros.id);
+
+   }) 
+
+   if (!post){
+    return <h1>Não Encontrada</h1>
+   }
+
+
+    return (   
+        <Routes>
+            <Route path='*'element={<PaginaPadrao/>}>
+                    <Route index element={          <PostModelo
+                        fotoCapa={`/assets/posts/${post.id}/capa.png`}
+                        titulo={post.titulo}
+                        >
+
+                    <div className="post-markdown-container">
+                        <ReactMarkdown >
+                        {post.texto}
+                        </ReactMarkdown>
+
+                    </div>
+                
+                    </PostModelo>} 
+                />
+            </Route>
+        </Routes>
+          
+    )
+}
